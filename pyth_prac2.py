2c16d0177f43c402438785adf320b333b1b2e606
Chanagouda-patil <patilchanagouda1995@gmail.com>
2022-09-06 09:12:47 +0530

    new program for practice

---
+++ pyth_prac2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number-1 :"))
+num2 = int(input("Enter a number-2 :"))
+print("Number-1 is", num1)
+print("Number-2 is", num2)
+if num1 > num2 :
+    print(num1, "is greater than", num2)
